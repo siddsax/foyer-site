f1c04499fdbb1f4ac5a2d808ecc47d0c133eb81f
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2021-10-28 17:54:36 +0530

    partway in adding action items

--- src/Components/ActionItemsDisplay/ActionItemsDisplay.jsx
+++ src/Components/ActionItemsDisplay/ActionItemsDisplay.jsx
@@ -6,17 +6,28 @@ export default function ActionItemsDisplay(props) {
   const { user, noteId } = props;
   const [actionItems, setActionItems] = useState([]);
   const db = firebase.firestore();
-  const docRef = db
-    .collection("ActionItems")
-    .where("noteId", "==", noteId)
-    .orderBy("date", "asc");
 
-  docRef.onSnapshot((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-      setActionItems((preVal) => [...preVal, doc.data()]);
+  const getActionItems = () => {
+    const docRef = db
+      .collection("ActionItems")
+      .where("noteId", "==", noteId)
+      .orderBy("date", "asc");
+
+    docRef.onSnapshot((querySnapshot) => {
+      querySnapshot.docChanges().forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+        setActionItems((preVal) => [...preVal, doc.data()]);
+      });
     });
-  });
+  };
+
+  useEffect(() => {
+    // getActionItems();
+  }, []);
+
+  useEffect(() => {
+    console.log(actionItems, "++++++++++++++++++");
+  }, [actionItems]);
 
   return (
     <div>
