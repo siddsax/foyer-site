711679bc2ce5fea3d49f6817ef41ce00ba1f0d07
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2021-10-31 07:16:27 +0530

    new editor and collaboration but no update to firebase

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./Editor.css";
@@ -7,63 +7,22 @@ import { QuillBinding } from "y-quill";
 import { WebrtcProvider } from "y-webrtc";
 import Quill from "quill";
 import QuillCursors from "quill-cursors";
-import DoUsername from "do_username";
+// import DoUsername from "do_username";
+import { WebsocketProvider } from "y-websocket";
+import { IndexeddbPersistence } from "y-indexeddb";
+import { usercolors, modules, formats, setUsername } from "./Editorhelpers";
 
 Quill.register("modules/cursors", QuillCursors);
-
-const ydoc = new Y.Doc();
-const ytext = ydoc.getText("quill");
-
-const provider = new WebrtcProvider("quill-demo-room", ydoc);
-const awareness = provider.awareness;
-
-export const usercolors = [
-  "#30bced",
-  "#6eeb83",
-  "#ffbc42",
-  "#ecd444",
-  "#ee6352",
-  "#9ac2c9",
-  "#8acb88",
-  "#1be7ff",
-];
-const myColor = usercolors[Math.floor(Math.random() * usercolors.length)];
-
-const modules = {
-  cursors: true,
-  toolbar: [
-    [{ header: [1, 2, false] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image"],
-  ],
-};
-
-const formats = [
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-];
 
 const EditorFoyer = (props) => {
   const { user, note, updateDB, value, setValue } = props;
   var quillRef, reactQuillRef, binding;
-  const setUsername = () => {
-    awareness.setLocalStateField("user", { name: user.email, color: myColor });
-  };
+  const myColor = usercolors[Math.floor(Math.random() * usercolors.length)];
+  const ydoc = new Y.Doc();
+  const ytext = ydoc.getText("quill");
+  const firstTime = useRef(true);
+
+  var provider, awareness, persistence;
   const onEditNote = (value) => {
     setValue(value);
     updateDB(note.title, JSON.stringify(value));
@@ -72,35 +31,35 @@ const EditorFoyer = (props) => {
   useEffect(() => {
     console.log(note);
     if (note) {
+      if (firstTime.current) {
+        console.log("first time");
+        firstTime.current = false;
+        provider = new WebsocketProvider(
+          // "wss://demos.yjs.dev",
+          "wss://limitless-thicket-34436.herokuapp.com/",
+          note.id,
+          ydoc
+        );
+        awareness = provider.awareness;
+        persistence = new IndexeddbPersistence(note.id, ydoc);
+        quillRef = reactQuillRef.getEditor();
+        binding = new QuillBinding(ytext, quillRef, provider.awareness);
+        setUsername({ user: user, awareness: awareness, myColor: myColor });
+
+        const strings = [];
+        awareness.getStates().forEach((state) => {
+          console.log(state);
+          if (state.user) {
+            strings.push(
+              `<div style="color:${state.user.color};">• ${state.user.name}</div>`
+            );
+          }
+        });
+      }
       setValue(JSON.parse(note.body));
     }
   }, [note]);
 
-  const updateText = (content, delta, source, editor) => {
-    console.log(content, delta, source, editor, value);
-  };
-
-  useEffect(() => {
-    quillRef = reactQuillRef.getEditor();
-    binding = new QuillBinding(ytext, quillRef, provider.awareness);
-    setUsername();
-
-    const strings = [];
-    awareness.getStates().forEach((state) => {
-      console.log(state);
-      if (state.user) {
-        strings.push(
-          `<div style="color:${state.user.color};">• ${state.user.name}</div>`
-        );
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      console.log("unmount");
-    };
-  }, []);
   return (
     <div className="editor">
       <ReactQuill
@@ -108,7 +67,7 @@ const EditorFoyer = (props) => {
           reactQuillRef = el;
         }}
         theme="snow"
-        onChange={updateText}
+        // onChange={updateText}
         modules={modules}
         formats={formats}
         defaultValue={"OK"}
